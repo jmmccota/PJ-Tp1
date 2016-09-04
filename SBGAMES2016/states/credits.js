156cc8ab171ebfa340c5fdb0e9c4ef55db5c2d8a
João <joao_marcos_cota@hotmail.com>
2016-09-03 22:48:52 -0300

    Creditos

--- SBGAMES2016/states/credits.js
+++ SBGAMES2016/states/credits.js
@@ -29,7 +29,7 @@ Credits.prototype = {
 
     addMenuOption: function (text, callback) {
         var optionStyle = { font: '30pt TheMinion', fill: 'black', align: 'left', stroke: 'rgba(0,0,0,0)', srokeThickness: 4 };
-        var txt = game.add.text(10, (this.optionCount * 80) + 450, text, optionStyle);
+        var txt = game.add.text(10, (this.optionCount * 80) +420, text, optionStyle);
 
         txt.stroke = "rgba(0,0,0,0";
         txt.strokeThickness = 4;
@@ -65,18 +65,20 @@ Credits.prototype = {
         //}
 
         var bg = game.add.sprite(0, 0, 'gameover-bg');
-        this.addCredit('Music', 'Kevin Macleod');
-        this.addCredit('Developer', 'Matt McFarland');
-        this.addCredit('Lorem Ipsum', 'Mipsem Dempsum');
-        this.addCredit('Caveats', 'Keyboard Cat');
+		this.addCredit('Este jogo foi desenvolvido por...','Creditos...')
+        this.addCredit('\n\nDaniel Gomes, Diego Haji,\nJo\u00e3o Marcos e Roberto Junior', 'Desenvolvedores');
+		this.addCredit('CEFET-MG campus Tim\u00f3teo','Em...');
+		this.addCredit('\n\nTal trabalho \u00e9 pr\u00e9-requisito para\n aprova\u00e7\u00e3o na disciplina de\n Programa\u00e7\u00e3o com Jogos','Para...');
+		this.addCredit('Andr\u00e9 Rodrigues da Cruz','Professor...');
+		this.addCredit('\n\n\nJavaScript em conjunto com \na API Canvas do HTML5.\n Utilizou-se folha de estilo CSS \ncomo parte do layout do jogo','Tecnologias....');
         this.addCredit('Phaser.io', 'Powered By');
         this.addCredit('for playing', 'Thank you');
-		this.addCredit('Este jogo foi desenvolvido por Daniel Gomes,\n Diego Haji, Jo\u00e3o Marcos e Roberto\n Junior, todos alunos graduandos em Engenharia de Computa\u00e7\u00e3o do CEFET-MG\n campus Tim\u00f3teo. Tal trabalho \u00e9 pr\u00e9-requisito para aprova\u00e7\u00e3o na disciplina de\n Programa\u00e7\u00e3o com Jogos, que possui como professor o Andr\u00e9 Rodrigues da Cruz.\n Utilizou-se para a constru\u00e7\u00e3o do jogo, a linguagem JavaScript em conjunto com a API Canvas do HTML5.\n Utilizou-se folha de estilo CSS como parte do layout do jogo.','Sobre o Jogo');
+
         this.addMenuOption('<= Back', function (e) {
             game.state.start("GameMenu");
         });
 
-        game.add.tween(bg).to({ alpha: 0 }, 20000, Phaser.Easing.Cubic.Out, true, 30000);
+        game.add.tween(bg).to({ alpha: 0 }, 30000, Phaser.Easing.Cubic.Out, true, 40000);
     }
 
 };
